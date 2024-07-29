2362d5e74e1dd436a1d6a2dc0e5acf9fc04a596f
Pranav Rajesh Waklekar <pranavwaklekar09@gmail.com>
2024-07-29 21:54:03 +0530

    optimized playlistHeader

--- app/src/main/java/com/clovermusic/clover/presentation/composable/playlistScreen/PlaylistHeader.kt
+++ app/src/main/java/com/clovermusic/clover/presentation/composable/playlistScreen/PlaylistHeader.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.FilledTonalButton
@@ -43,11 +46,7 @@ import com.clovermusic.clover.util.Parsers
 fun PlaylistHeader(
     playlist: Playlist
 ) {
-    val more = painterResource(id = R.drawable.more2)
-    val shuffle = painterResource(id = R.drawable.shuffle1)
-    val play = painterResource(id = R.drawable.play2)
-    val playName = stringResource(id = R.string.play)
-    val shuffleText = stringResource(id = R.string.shuffle)
+
     Surface(
         color = Color.Transparent,
         modifier = Modifier
@@ -78,136 +77,155 @@ fun PlaylistHeader(
                         .padding(horizontal = 14.dp)
                         .weight(1f)
                 ) {
-                    Text(
-                        text = "${playlist.tracks.size} songs • ${
-                            Parsers.parseDurationHoursMinutes(
-                                playlist.tracks.sumOf { it.durationMs })
-                        }",
-                        style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.secondary
-                    )
-
-                    Text(
-                        text = playlist.name,
-                        style = MaterialTheme.typography.titleLarge,
-                        color = MaterialTheme.colorScheme.primary,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .wrapContentSize()
-                    ) {
-                        Text(
-                            text = "${playlist.owner.display_name} ${if (playlist.followers!! < 1) " • " else ""}",
-                            style = MaterialTheme.typography.labelMedium,
-                            color = MaterialTheme.colorScheme.tertiary
-                        )
-                        if (playlist.followers < 1) {
-                            Text(
-                                text = "${playlist.followers} followers",
-                                style = MaterialTheme.typography.labelSmall,
-                                color = MaterialTheme.colorScheme.tertiary
-                            )
-                        }
-                    }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        IconButton(
-                            onClick = { /*TODO*/ },
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.AddCircle,
-                                contentDescription = "followed",
-                                modifier = Modifier
-                                    .fillMaxSize(0.7f)
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                painter = more,
-                                contentDescription = "More",
-                                modifier = Modifier
-                                    .fillMaxSize(0.7f)
-                            )
-                        }
-                    }
+                    PlaylistInfo(playlist)
                 }
+
             }
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
+            Buttons()
+
+        }
+    }
+}
+
+@Composable
+fun PlaylistInfo(playlist: Playlist){
+    val more = painterResource(id = R.drawable.more2)
+
+    Text(
+        text = "${playlist.tracks.size} songs • ${
+            Parsers.parseDurationHoursMinutes(
+                playlist.tracks.sumOf { it.durationMs })
+        }",
+        style = MaterialTheme.typography.labelSmall,
+        color = MaterialTheme.colorScheme.secondary
+    )
+
+    Text(
+        text = playlist.name,
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.primary,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis
+    )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .wrapContentSize()
+    ) {
+        Text(
+            text = "${playlist.owner.display_name} ${if (playlist.followers!! < 1) " • " else ""}",
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.tertiary
+        )
+        if (playlist.followers < 1) {
+            Text(
+                text = "${playlist.followers} followers",
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        }
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(
+            onClick = { /*TODO*/ },
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.AddCircle,
+                contentDescription = "followed",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(vertical = 10.dp, horizontal = 14.dp)
-                    .background(color = Color.Transparent)
-            ) {
-                FilledTonalButton(
-                    onClick = {},
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-                    elevation = ButtonDefaults.elevatedButtonElevation(3.dp),
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .fillMaxHeight(0.2f)
-                )  {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxSize()
-                    ) {
-                        Icon(
-                            painter = shuffle,
-                            contentDescription = "Shuffle button",
-                            modifier = Modifier
-                                .padding(horizontal = 20.dp)
-                                .fillMaxHeight(0.5f)
-                                .weight(0.7f)
-                        )
-                        Text(
-                            text = shuffleText,
-                            modifier = Modifier
-                                .weight(0.8f)
-                        )
-                    }
-                }
-                OutlinedButton(
-                    onClick = {},
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-                    elevation = ButtonDefaults.elevatedButtonElevation(3.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.5f),
-                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline)
-                )  {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxSize()
-                    ) {
-                        Icon(
-                            painter = play,
-                            contentDescription = "Play button",
-                            modifier = Modifier
-                                .padding(horizontal = 20.dp)
-                                .fillMaxHeight(0.5f)
-                                .weight(0.2f)
-                        )
-                        Text(
-                            text = playName,
-                            modifier = Modifier
-                                .weight(0.2f)
-                        )
-                    }
+                    .fillMaxSize(0.7f)
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = more,
+                contentDescription = "More",
+                modifier = Modifier
+                    .fillMaxSize(0.7f)
+            )
+        }
+    }
 
-                }
+}
+@Composable
+fun Buttons(){
+    val shuffle = painterResource(id = R.drawable.shuffle1)
+    val play = painterResource(id = R.drawable.play2)
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(vertical = 8.dp, horizontal = 14.dp)
+            .background(color = Color.Transparent)
+    ) {
+        FilledTonalButton(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+            elevation = ButtonDefaults.elevatedButtonElevation(3.dp),
+            modifier = Modifier
+                .height(64.dp)
+                .fillMaxWidth(0.5f)
+        )  {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Icon(
+                    painter = shuffle,
+                    contentDescription = "Shuffle button",
+                    modifier = Modifier
+                        .fillMaxHeight(0.5f)
+                        .size(32.dp)
+                )
+                Text(
+                    text = "Shuffle",
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                )
             }
+        }
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surface),
+            elevation = ButtonDefaults.elevatedButtonElevation(3.dp),
+            modifier = Modifier
+                .height(64.dp)
+                .fillMaxWidth()
+        )  {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+
+            ) {
+                Icon(
+                    painter = play,
+                    contentDescription = "Play button",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .fillMaxHeight(0.5f)
+
+                )
+                Text(
+                    text = "Play",
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                )
+            }
+
         }
     }
 }
