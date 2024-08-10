20de2fc11dee31cf0c83f2487801ce6c73e48a1f
swaraj <swarajgaikwad1925@gmail.com>
2024-08-11 02:10:28 +0530

    optimized to slider
    - previously we used to make call to Spotify app remote cautiously but which cozed ui to freeze but not we predict the value

--- app/src/main/java/com/clovermusic/clover/domain/usecase/playback/RemotePlaybackHandlerUseCase.kt
+++ app/src/main/java/com/clovermusic/clover/domain/usecase/playback/RemotePlaybackHandlerUseCase.kt
@@ -1,15 +1,19 @@
 package com.clovermusic.clover.domain.usecase.playback
 
+import android.util.Log
 import com.clovermusic.clover.data.spotify.appRemote.SpotifyAppRemoteRepository
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.PlayerState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RemotePlaybackHandlerUseCase @Inject constructor(
@@ -23,38 +27,16 @@ class RemotePlaybackHandlerUseCase @Inject constructor(
     private val _playbackPosition = MutableStateFlow(0L)
     val playbackPosition: StateFlow<Long> = _playbackPosition.asStateFlow()
 
+
+    private var positionUpdateJob: Job? = null
+    private var lastUpdateTime: Long = 0
     private var isPlaying: Boolean = false
 
     suspend fun initialize() {
         if (remote == null) {
             remote = appRemoteRepository.getConnectedAppRemote()
             startPlayerStateUpdates()
-        }
-    }
-
-    private fun startPlayerStateUpdates() {
-        remote?.playerApi?.subscribeToPlayerState()?.setEventCallback { state ->
-            _playerState.value = state
-            isPlaying = !state.isPaused
-        }
-    }
-
-    fun updatePlaybackPosition() {
-        CoroutineScope(Dispatchers.IO).launch {
-            remote?.playerApi?.subscribeToPlayerState()?.setEventCallback { state ->
-                _playbackPosition.value = state.playbackPosition
-            }
-            delay(1000)
-        }
-    }
-
-
-    suspend fun performRemoteAction(action: suspend (SpotifyAppRemote) -> Unit) {
-        try {
-            val remote = appRemoteRepository.getConnectedAppRemote()
-            action(remote)
-        } catch (e: Exception) {
-            throw e
+            startPositionUpdates()
         }
     }
 
@@ -65,7 +47,48 @@ class RemotePlaybackHandlerUseCase @Inject constructor(
     fun skipToPrevious() = remote?.playerApi?.skipPrevious()
     fun toggleShuffle() = remote?.playerApi?.toggleShuffle()
     fun toggleRepeat() = remote?.playerApi?.toggleRepeat()
-    fun seekTo(position: Long) = remote?.playerApi?.seekTo(position)
-    fun getCurrentPlaybackPosition(): Long = playerState.value?.playbackPosition ?: 0L
+
+    suspend fun seekTo(position: Long) {
+        withContext(Dispatchers.IO) {
+            remote?.playerApi?.seekTo(position)
+            _playbackPosition.value = position
+            lastUpdateTime = System.currentTimeMillis()
+        }
+    }
+
+    private fun startPlayerStateUpdates() {
+        remote?.playerApi?.subscribeToPlayerState()?.setEventCallback { state ->
+            Log.d("RemotePlaybackHandler", "Player state updated")
+            _playerState.value = state
+            isPlaying = !state.isPaused
+            _playbackPosition.value = state.playbackPosition
+            lastUpdateTime = System.currentTimeMillis()
+        }
+    }
+
+    //    Predict the value of slider based on the current playback position
+    private fun startPositionUpdates() {
+        positionUpdateJob?.cancel()
+        positionUpdateJob = CoroutineScope(Dispatchers.Default).launch {
+            while (isActive) {
+                if (isPlaying) {
+                    val currentTime = System.currentTimeMillis()
+                    val timeDifference = currentTime - lastUpdateTime
+                    _playbackPosition.value += timeDifference
+                    lastUpdateTime = currentTime
+                }
+                delay(100)
+            }
+        }
+    }
+
+    private suspend fun performRemoteAction(action: suspend (SpotifyAppRemote) -> Unit) {
+        try {
+            val remote = appRemoteRepository.getConnectedAppRemote()
+            action(remote)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 
 }
