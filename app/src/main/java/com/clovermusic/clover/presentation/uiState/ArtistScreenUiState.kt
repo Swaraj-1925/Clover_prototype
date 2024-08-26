4ad8b73890d60365b71a4f01de8864aa7443bc16
Pranav Rajesh Waklekar <pranavwaklekar09@gmail.com>
2024-08-26 10:46:53 +0530

    Implemented AlbumScreen
    Added Header , Content and AlbumScreen data

--- app/src/main/java/com/clovermusic/clover/presentation/uiState/ArtistScreenUiState.kt
+++ app/src/main/java/com/clovermusic/clover/presentation/uiState/ArtistScreenUiState.kt
@@ -1,6 +1,8 @@
 package com.clovermusic.clover.presentation.uiState
 
+import com.clovermusic.clover.data.local.entity.AlbumEntity
 import com.clovermusic.clover.data.local.entity.ArtistsEntity
+import com.clovermusic.clover.data.local.entity.relations.AlbumWithTrack
 import com.clovermusic.clover.data.local.entity.relations.ArtistWithAlbums
 import com.clovermusic.clover.data.local.entity.relations.TrackWithArtists
 
